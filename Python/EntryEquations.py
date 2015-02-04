bd02f7db0feae03789e2b10e35f00b76057b0f5b
Connor Noyes <aero.noyes@gmail.com>
2015-02-04 14:30:17 -0800

    Added an entry vehicle class with default parameters similar to MSL. Updated entry equations but vinh's equations are still unfinished. The planet class now has an atmosphere model method, currently functional for Mars only.

--- Python/EntryEquations.py
+++ Python/EntryEquations.py
@@ -2,23 +2,47 @@
 from math import sin, cos
 import numpy as np
 #3DOF, Non-rotating Earth (i.e. Coriolis terms are excluded)
-def entry_3dof(x,t):
-	dh = v*sin(gamma)
-	dv = -D - g*sin(gamma)
-	dgamma = L/V*cos(sigma) + cos(gamma)*(v/r - g)
-	return np.array([]})
+def entry_3dof(x, t, planet, vehicle):
+    r,theta,phi,v,gamma,psi = x
+    
+    g = planet.mu/r**2
+    h = r - planet.radius
+    rho,a = planet.atmosphere(h)
+    M = v/a
+    cD,cL = vehicle.aerodynamic_coefficients(M)
+    f = 0.5*rho*vehicle.area*v**2/vehicle.mass
+    L = f*cL
+    D = f*cD
+    
+    dh = v*sin(gamma)
+    dtheta = v*cos(gamma)*cos(psi)/r/cos(phi)
+    dphi = v*cos(gamma)*sin(psi)/r
+    dv = -D - g*sin(gamma)
+    dgamma = L/V*cos(sigma) + cos(gamma)*(v/r - g)
+    dpsi = -L*sin(sigma)/v/cos(gamma) - v*cos(gamma)*cos(psi)*tan(phi)/r
+    return np.array([dh, dtheta, dphi, dv, dgamma, dpsi])
 	
 #3DOF, Rotating Earth Model
 def entry_vinhs(x,t):
-	
-	return
+
+    return
 
 #2DOF, Longitudinal Model
-def entry_2dof(x,t):
+def entry_2dof(x, t, planet, vehicle):
+    r,s,v,gamma = x
+    
+    g = planet.mu/r**2
+    h = r - planet.radius
+    rho,a = planet.atmosphere(h)
+    M = v/a
+    cD,cL = vehicle.aerodynamic_coefficients(M)
+    f = 0.5*rho*vehicle.area*v**2/vehicle.mass
+    L = f*cL
+    D = f*cD
+    
+    dh = v*sin(gamma)
+    ds = v*cos(gamma)
+    dv = -D - g*sin(gamma)
+    dgamma = L/V*cos(sigma) + cos(gamma)*(v/r - g)
 
-	dh = v*sin(gamma)
-	ds = v*cos(gamma)
-	dv = -D - g*sin(gamma)
-	dgamma = L/V*cos(sigma) + cos(gamma)*(v/r - g)
-	
-	return np.array([dh,ds,dv,dgamma])
+return np.array([dh,ds,dv,dgamma])
--- Python/Planet.py
+++ Python/Planet.py
@@ -1,12 +1,14 @@
+from math import exp
+
 class Planet:
-    def __init__(self, name):
+    def __init__(self, name = 'Mars'):
         self.name = name.capitalize()
         
         if self.name == 'Mercury':
             self.radius = float('nan')  # equatorial radius, m
-            self.omega = float('nan') 	# angular rate of planet rotation, rad/s
+            self.omega = float('nan')     # angular rate of planet rotation, rad/s
             self.mu = float('nan')      # gravitational parameter, m^3/s^2
-
+            print 'Planet model not yet implemented!'
         elif self.name == 'Venus':
             self.radius = float('nan')    
             self.omega = float('nan')
@@ -19,7 +21,7 @@ class Planet:
             
         elif self.name == 'Mars':
             self.radius = 3397e3    
-            self.omega = 7.095e-5 	
+            self.omega = 7.095e-5     
             self.mu = 4.2830e13
         
         elif self.name == 'Saturn':
@@ -44,4 +46,22 @@ class Planet:
             
         else:
             print 'Input planet name, '+ self.name +', is not valid'
-		
+        
+    def atmosphere(self, h):
+        if self.name == 'Mars':
+        #Density computation:
+            rho0 = 0.0158
+            scale_height = 9354.5
+            rho = rho0*exp(-h/scale_height)
+        # Local speed of sound computation:
+            coeff = [223.8, -0.2004e-3, -1.588e-8, 1.404e-13]
+            a = 0
+            i = 0
+            for c in coeff:
+                a = a + c*h**i
+                i = i+1
+            
+            return rho,a
+        else:
+          print 'Atmosphere model not yet implemented!'
+          return float('nan'), float('nan')
