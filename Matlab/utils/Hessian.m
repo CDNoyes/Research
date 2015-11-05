6b0a54e3ffa56afe1118ac8144d7d97e46d40312
Connor Noyes <aero.noyes@gmail.com>
2015-11-05 12:02:29 -0800

    Extended the capabilities of the Hessian approximation function to allow for vector-valued functions. Added a vector-valued function to the test function.

--- Matlab/utils/Hessian.m
+++ Matlab/utils/Hessian.m
@@ -1,16 +1,32 @@
 function H = Hessian( fun, x, stepSize )
-%HESSIAN Numerically computes the hessian of a scalar function. 
+%HESSIAN Numerically computes the hessian of a function.
 %   The approximation is performed by using a complex step method to
-%   estimate the gradient and using a central difference scheme to compute
-%   the hessian. If the gradient of the function is known analytically,
-%   ComplexDiff should be used on the gradient to obtain a more accurate
-%   solution.
+%   estimate the gradient/jacobian and using a central difference scheme to
+%   compute the hessian. If the gradient/jacobian of the function is known
+%   analytically, ComplexDiff should be used on the gradient to obtain a
+%   more accurate solution.
 
 df = @(x) ComplexDiff(fun,x);
-if nargin < 3
-    H = CentralDiff(df,x);
-else
-    H = CentralDiff(df,x,stepSize);
+fx = fun(x);
+if isscalar(fx)
+    if nargin < 3
+        H = CentralDiff(df,x);
+    else
+        H = CentralDiff(df,x,stepSize);
+    end
+else %Vector valued function
+    n = length(fx);
+    m = length(x);
+    H = nan(m,m,n);
+    for i = 1:n
+        H(:,:,i) = CentralDiff(@(X)wrapper(df,X,i),x);
+    end
+    
 end
 end
 
+function dfi = wrapper(df,x,index)
+dvalue = df(x); %A matrix
+dfi = dvalue(index,:); %The gradient of the scalar "index" function wrt the the states
+
+end
--- Matlab/utils/testNumericalDifferentiation.m
+++ Matlab/utils/testNumericalDifferentiation.m
@@ -27,3 +27,18 @@ forwardError = norm(exactGradient(testPoint) - ForwardDiff(testFunction,testPoin
 hessianError = norm(exactHessian(testPoint)  - Hessian(testFunction,testPoint));
 
 %% Vector-valued function of 2 variables:
+clear; clc;
+testFunction = @(x) [x(2);-x(1)+0.25*(1-x(1)^2)*x(2)];
+exactJacobian = @(x) [0, 1; -1-2*0.25*x(1)*x(2)  0.25*(1-x(1)^2) ];
+% exactHessian = @(x) [0, exp(x(2)); exp(x(2)), x(1)*exp(x(2))];
+
+testPoint = [1;1];
+x = testPoint;
+exactHessian(1:2,1:2,1) = zeros(2);
+exactHessian(1:2,1:2,2) = [-0.5*x(2),-0.5*x(1); -0.5*x(1),0];
+
+complexError = norm(exactJacobian(testPoint) - ComplexDiff(testFunction,testPoint));
+centralError = norm(exactJacobian(testPoint) - CentralDiff(testFunction,testPoint));
+forwardError = norm(exactJacobian(testPoint) - ForwardDiff(testFunction,testPoint));
+numHessian = Hessian(testFunction,testPoint);
+hessianError = norm(exactHessian(:,:,1)-numHessian(:,:,1))+norm(exactHessian(:,:,2)-numHessian(:,:,2));
