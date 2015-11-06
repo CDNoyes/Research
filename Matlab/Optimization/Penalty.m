e471e49b67ca7eb052d06f5cdafd159e0d4a4e4e
Connor Noyes <aero.noyes@gmail.com>
2015-11-06 15:12:36 -0800

    Updated Penalty to allow an arbitrary number of constraints. The method works surprisingly well even with equality constraints. A future update should allow user input tolerances and max number of iterations. I changed the way the penalty factor is initialized. There are probably superior ways to increase the penalty at each iteration.

--- Matlab/Optimization/Penalty.m
+++ Matlab/Optimization/Penalty.m
@@ -1,19 +1,19 @@
-function [x,fval,funcCount] = Penalty(f,c,x0)
+function [x,fval,funcCount,S] = Penalty(f,c,x0)
 %Implements a penalty method to solve constrained optimization. This
 %implementation uses MATLAB's fminsearch as the unconstrained optimizer.
 
-sigma = 0.01;
+sigma = f(x0)/norm(min(0,c(x0)).^2);
+S.initial = sigma;
 x = x0;
 xold = 1e6;
-tol = 1e-5;
+tol = 1e-3;
 funcCount = 0;
 while abs(x-xold) > tol
-phi = @(x) f(x) + sigma*min(0,c(x))^2;
-
-xold = x;
-[x,fval,~,output] = fminsearch(phi,xold);
-sigma = sigma*10;
-funcCount = output.funcCount + funcCount;
+    phi = @(x) f(x) + sum(sigma*min(0,c(x)).^2);
+    xold = x;
+    [x,fval,~,output] = fminsearch(phi,xold);
+    sigma = sigma*10;
+    funcCount = output.funcCount + funcCount;
 end
-
+S.final = sigma/10; %Since it gets multiplied by 10 before actually being used again
 end
--- Matlab/Optimization/Rosenbrock.m
+++ Matlab/Optimization/Rosenbrock.m
@@ -7,4 +7,5 @@ f = @(x) 100*(x(2)-x(1)^2)^2 + (1-x(1))^2;
 
 [x,fval,flag,output] = fminsearch(f,[0;0]);
 
-[x_constrained, f_constrained, f_evals] = Penalty(f,@(x) x(1)-x(2)-1,[0;0]);
+c = @(x) [x(2); -x(2)];
+[x_constrained, f_constrained, f_evals, penalty] = Penalty(f,c,[10;10]);
