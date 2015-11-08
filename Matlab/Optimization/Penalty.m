9197076c59738b995918b6fcfb5a4c24129ee690
Connor Noyes <aero.noyes@gmail.com>
2015-11-08 12:48:44 -0800

    Added some necessary checks to the Penalty method. If the function value was 0 at the initial point then no penalty was applied and the constraints were not satisfied. Similarly if the initial point was feasible, the penalty was infinite and the problem became ill-posed.

--- Matlab/Optimization/Penalty.m
+++ Matlab/Optimization/Penalty.m
@@ -1,17 +1,24 @@
 function [x,fval,funcCount,S] = Penalty(f,c,x0)
 %Implements a penalty method to solve constrained optimization. This
 %implementation uses MATLAB's fminsearch as the unconstrained optimizer.
-
-sigma = f(x0)/norm(min(0,c(x0)).^2);
+c0 = norm(min(0,c(x0)).^2);
+if ~c0
+    c0 = 1;
+end
+f0 = f(x0);
+if ~f0
+    f0 = 1;
+end
+sigma = f0/c0;
 S.initial = sigma;
 x = x0;
 xold = 1e6;
-tol = 1e-3;
+tol = 1e-6;
 funcCount = 0;
 while abs(x-xold) > tol
     phi = @(x) f(x) + sum(sigma*min(0,c(x)).^2);
     xold = x;
-    [x,fval,~,output] = fminsearch(phi,xold);
+    [x,fval,flag,output] = fminsearch(phi,xold);
     sigma = sigma*10;
     funcCount = output.funcCount + funcCount;
 end
--- Matlab/Optimization/Rosenbrock.m
+++ Matlab/Optimization/Rosenbrock.m
@@ -8,4 +8,4 @@ f = @(x) 100*(x(2)-x(1)^2)^2 + (1-x(1))^2;
 [x,fval,flag,output] = fminsearch(f,[0;0]);
 
 c = @(x) [x(2); -x(2)];
-[x_constrained, f_constrained, f_evals, penalty] = Penalty(f,c,[10;10]);
+[x_constrained, f_constrained, f_evals, penalty] = Penalty(f,c,[1;1]);
