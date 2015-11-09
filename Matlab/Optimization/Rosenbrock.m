4f3dd4c18e1f8ee1e8acf01f87ace49e96ac9b80
Connor Noyes <CDNoyes@users.noreply.github.com>
2015-11-08 20:23:24 -0800

    Tried two equality constraints with the Rosenbrock function, Penalty still works well. I ought to try other standard optimization functions.

--- Matlab/Optimization/Rosenbrock.m
+++ Matlab/Optimization/Rosenbrock.m
@@ -7,5 +7,5 @@ f = @(x) 100*(x(2)-x(1)^2)^2 + (1-x(1))^2;
 
 [x,fval,flag,output] = fminsearch(f,[0;0]);
 
-c = @(x) [x(2); -x(2); x(1)-.2];
+c = @(x) [x(2); -x(2); x(1); -x(1)];
 [x_constrained, f_constrained, f_evals, penalty] = Penalty(f,c,[1;1]);
