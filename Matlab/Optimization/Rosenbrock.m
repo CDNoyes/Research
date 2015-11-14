f8e4fbbf988391c44a77eec866b3595bb419dcc6
Connor Noyes <aero.noyes@gmail.com>
2015-11-13 16:45:19 -0800

    Trying more constraints.

--- Matlab/Optimization/Rosenbrock.m
+++ Matlab/Optimization/Rosenbrock.m
@@ -5,7 +5,7 @@ f = @(x) 100*(x(2)-x(1)^2)^2 + (1-x(1))^2;
 
 % [x,fopt,iter] = TrustRegion(f,[0;0]);
 
-[x,fval,flag,output] = fminsearch(f,[0;0]);
+[x,fval,flag,output] = fminsearch(f,[-1;1]);
 
-c = @(x) [x(2); -x(2); x(1); -x(1)];
-[x_constrained, f_constrained, f_evals, penalty] = Penalty(f,c,[1;1]);
+c = @(x) [x(2); -x(2); x(1)];
+[x_constrained, f_constrained, f_evals, penalty] = Penalty(f,c,[-1;1]);
