a4f2665c03a86b509ac05cebaedeb46e726b64ad
Connor Noyes <aero.noyes@gmail.com>
2015-11-19 16:58:33 -0800

    Adding a method that integrates bank angle dynamics as a second order system that respects limits on rate and acceleration. Only a shell currently, no functionality yet.

---
+++ Matlab/EntryGuidance/BankAngleDynamics.m
@@ -0,0 +1,9 @@
+function dx = BankAngleDynamics(t,x,profile)
+
+Kp = 0.5;
+Kd = 0.9;
+
+
+
+
+end
--- Matlab/EntryGuidance/EntryForces.m
+++ Matlab/EntryGuidance/EntryForces.m
@@ -6,16 +6,16 @@ if nargin < 4 || isempty(ScaleFactor)
 end
 
 %Constants:
-r_eq = planetModel.radiusEquatorial;      % m
-
+r_eq = planetModel.radiusEquatorial;        % m
+mu = planetModel.mu;                        % gravitational parameter, m^3/s^2
 %Vehicle Parameters
-S = vehicleModel.area; %reference wing area, m^2
-m = vehicleModel.mass; %mass, kg
+S = vehicleModel.area;                      % reference wing area, m^2
+m = vehicleModel.mass;                      % mass, kg
 
 r = x(1);       %vehicle radius, m
 V = x(4);       %velocity, m/s
 
-mu = 4.2830e13;     % gravitational parameter, m^3/s^2
+
 g = mu/r^2/ScaleFactor.velocity^2/ScaleFactor.radius;      % gravity, m/s^2
 
 h = r*ScaleFactor.radius-r_eq;                             % Altitude
