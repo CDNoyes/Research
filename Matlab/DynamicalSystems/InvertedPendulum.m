ccbff8be789a27eb3bea95af9d76842065c9b05c
Connor Noyes <aero.noyes@gmail.com>
2015-11-24 15:03:23 -0800

    Added an inverted pendulum model to the available dynamical systems.

---
+++ Matlab/DynamicalSystems/InvertedPendulum.m
@@ -0,0 +1,12 @@
+function [fIP,jIP] = InvertedPendulum()
+
+    fIP = @(t,x,u) [x(2); ControlWrapper(t,u)-4*sin(x(1))];
+    jIP = @InvertedPendulumJacobian;
+end
+
+function dfdx = InvertedPendulumJacobian(x,u)
+
+dfdx = [0, 1;-4*cos(x(1)) 0];
+dfdu = [0;1];
+
+end
