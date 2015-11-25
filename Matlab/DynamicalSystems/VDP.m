c4e02dfff018a5985de15a6ae69fc6d32b577020
Connor Noyes <CDNoyes@users.noreply.github.com>
2015-11-25 14:01:17 -0800

    After some consideration, I changed the VDP dynamical system is several ways: the function does not take a value of mu, it must be passed to the dynamics, jacobian, and hessian. The jacobian and hessian now include the derivatives of the state equations with respect to the control and the parameter mu.

--- Matlab/DynamicalSystems/VDP.m
+++ Matlab/DynamicalSystems/VDP.m
@@ -5,13 +5,17 @@
 %   via Matlab's builtin integrators.
 %
 % dynamics: f(time,state,control)
-% jacobian: j(state)
+% jacobian: j(state,control)
 
-function [fVDP,jVDP,hVDP] = VDP(mu)
+function [fVDP,jVDP,hVDP] = VDP()
 
-fVDP = @(t,x,u) vdpDynamics(t,x,u,mu);
-jVDP = @(x,u) vdpJacobian(x,u,mu);
-hVDP = @(x,u) vdpHessian(x,u,mu);
+% fVDP = @(t,x,u) vdpDynamics(t,x,u,mu);
+% jVDP = @(x,u) vdpJacobian(x,u,mu);
+% hVDP = @(x,u) vdpHessian(x,u,mu);
+
+fVDP = @vdpDynamics;
+jVDP = @vdpJacobian;
+hVDP = @vdpHessian;
 
 end
 
@@ -24,13 +28,16 @@ end
 
 function J = vdpJacobian(x,u,mu)
 
-J = [0, 1
-    -1-2*mu*x(1)*x(2), mu*(1-x(1).^2)];
+J = [0, 1, 0, 0
+    -1-2*mu*x(1)*x(2), mu*(1-x(1).^2), 1,(1-x(1).^2)*x(2)];
 
 end
 
-function H = vdpHessian(x,u,mu)
-h{2} = [-2*mu*x(2),-2*mu*x(1); -2*mu*x(1),0];
-h{1} = [0,0; 0, 0];
+function [H,h] = vdpHessian(x,u,mu)
+h{2} = [-2*mu*x(2),-2*mu*x(1),0,-2*x(1)*x(2);
+        -2*mu*x(1),0,0,(1-x(1).^2)
+        0,0,0,0
+        -2*x(1)*x(2),(1-x(1).^2),0,0];
+h{1} = zeros(4,4);
 H = sparse(blkdiag(h{:}));
 end
