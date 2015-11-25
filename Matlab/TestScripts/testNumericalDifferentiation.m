909ef4cdaa56ceb6ccdf006b8f98a56a9403f2f1
Connor Noyes <CDNoyes@users.noreply.github.com>
2015-11-25 14:41:44 -0800

    Added another test using numerical diff on VDP system.

--- Matlab/TestScripts/testNumericalDifferentiation.m
+++ Matlab/TestScripts/testNumericalDifferentiation.m
@@ -41,3 +41,23 @@ centralError = norm(exactJacobian(testPoint) - CentralDiff(testFunction,testPoin
 forwardError = norm(exactJacobian(testPoint) - ForwardDiff(testFunction,testPoint));
 numHessian = Hessian(testFunction,testPoint);
 hessianError = norm([norm(exactHessian(:,:,1)-numHessian(:,:,1)),norm(exactHessian(:,:,2)-numHessian(:,:,2))]);
+
+%% VDP Oscillator: vector valued with two states, one control, and one parameter
+clear; clc;
+[F,J,H] = VDP();
+[dim,ind] = Dimension(2,1,1);
+
+x0 = [3;5];
+u0 = 0;
+mu = 0.25;
+X0 = [x0;u0;mu];
+
+%Analytical Results
+f = F(0,x0,u0,mu);
+j = J(x0,u0,mu);
+h = H(x0,u0,mu);
+
+%Numerical Results
+F1 = @(X) F(0,X(ind.state),X(ind.control),X(ind.parameter));
+jn = ComplexDiff(F1,X0);
+hn = Hessian(F1,X0);
