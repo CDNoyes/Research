dbed17d850e35b309df1c4da5929f3a8cd3ecd6d
Connor Noyes <aero.noyes@gmail.com>
2015-11-27 14:01:59 -0800

    More work on CTDDP, getting closer to being complete.

--- Matlab/optimalControl/DynamicProgramming/CTDDP.m
+++ Matlab/optimalControl/DynamicProgramming/CTDDP.m
@@ -17,17 +17,24 @@ n = OCP.dimension.state;
 m = OCP.dimension.control;
 p = OCP.dimension.adjoint;
 
-nPoints = 100;
-t = linspace(0,tf,nPoints);
-u = zeros(m,nPoints); %Initial control guess of all zeros.
-x0 = OCP.state.initial;
-lambda = zeros(p,1); %Initial adjoint guess of zeros
-fixed = ~OCP.free.finalState; %Logical indices of the state vector elements that are fixed at tfinal, length p
+[~,OCP.ind] = Dimension(n,m,0);
 
+nPoints = 100;
+t = linspace(0,OCP.bounds.upper.finalTime,nPoints);
+u = zeros(m,nPoints); %Initial control guess of all zeros.
+x0 = OCP.bounds.upper.initialState;
+fixed = ~OCP.free.finalState; %Logical indices of the state vector elements that are fixed at tfinal, length p
+if p == 0 && any(fixed) %This means we treat constraints softly
+    p = sum(fixed);
+    OCP.dimension.adjoint = p;
+    softConstraints = true; 
+end
+lambda = zeros(p,1);
 constraintGrad = Psi_X(fixed,p,n);
+
 if OCP.order == 0
     eta = .3; % A parameter to be set
-    gamma = 0.1;
+    gamma = 0.4;
 else
     eta = 1;
     gamma = 1;
@@ -37,36 +44,43 @@ tol = 1e-4;
 iter = 0;
 iterMax = 40;
 constraintViolation = tol+1;
+opt = odeset('AbsTol',1e-8,'RelTol',1e-8);
 % The DDP loop
 
 while constraintViolation > tol && iter < iterMax
     
     %Integrate dynamics forward with current control
-    [t,x] = ode45(OCP.dynamics,t,x0,[], @(T) interp1(t,u,T));
-    constraintViolation = max(abs(x(end,fixed)-OCP.bounds.finalState(fixed)'));
+    [~,x] = ode45(OCP.dynamics,t,x0,[], @(T) interp1(t,u,T));
+    constraintViolation = max(abs(x(end,fixed)-OCP.bounds.upper.finalState(fixed)'));
     
     %Compute V and its partials, eq. 28, at tFinal
-    V = OCP.cost.mayer(t(end),x(end,:)) +...
-        (x(end,fixed)-OCP.bounds.finalState(fixed)')*lambda;        % Scalar
+    V = OCP.cost.mayer(t(end),x(end,:)') +...
+        (x(end,fixed)-OCP.bounds.upper.finalState(fixed)')*lambda;        % Scalar
     Vx = ComplexDiff(@(X)OCP.cost.mayer(t(end),X),x(end,:))...
-        + constraintGrad'*lambda;                                   % Row vector of lenth n
-    Vlambda = (x(end,fixed)-OCP.bounds.finalState(fixed)');         % Row vector of length p
-    Vxx = Hessian(@(X)OCP.cost.mayer(t(end),X),x(end,:));           % nxn matrix
+        + lambda'*constraintGrad;                                   % Row vector of lenth n
+    Vlambda = (x(end,fixed)-OCP.bounds.upper.finalState(fixed)');         % Row vector of length p
+    Vxx = Hessian(@(X)OCP.cost.mayer(t(end),X),x(end,:)');           % nxn matrix
     Vxlambda = constraintGrad;                                      % pxn matrix
     Vlambda2 = zeros(p);                                            % pxp matrix
     
     %Integrate the Value functions backward
-    Vf = [V;Vx(:);Vlambda(:);Vxx(:);Vxlambda(:);Vlambda2(:)];
-    [tb,Vstate] = ode45(valueFunction,linspace(tf,0,nPoints),Vf,[]);
+    Vf = [V;Vx(:);Vlambda(:);Vxx(:);Vlambda2(:);Vxlambda(:)];
+    [tb,Vstate] = ode45(@valueFunction,linspace(t(end),0,nPoints),Vf,opt,OCP,@(T) interp1(t,x,T),@(T) interp1(t,u,T));
     
     %Parse Vstate
-    [V,Vx,Vl,Vxx,Vll,Vxl] = parseValueStates(Vstate,ocp);
+    [V,Vx,Vl,Vxx,Vll,Vxl] = parseValueStates(Vstate,OCP);
     
     %Compute the delta lambda quantity
-    dlambda = -eta*inv(Vll{1})*Vlambda;
+    if ~softConstraints
+        dlambda = -eta*inv(Vll{1})*Vlambda';
+    else
+        dlambda = zeros(p,1);
+    end
     
     %Compute gains for the whole trajectory
     for i = nPoints:-1:1
+        %Compute H,L,F here
+        [H,L,F] = computePartials(ocp,x(i,:),u(:,i));
         [l{i},Kx{i},Kl{i}] = computeGains(H,L,F,Vx,Vxx,Vxl);
     end
     %Integrate the linearize dynamics forward using new delta-control
@@ -74,7 +88,8 @@ while constraintViolation > tol && iter < iterMax
     %Computer delta-control
     
     %Update control and adjioint
-    u = u+lambda*du;
+    u = u+gamma*du;
+
     lambda = lambda + dlambda;
     iter = iter+1; %Increment the counter and continue the loop!
 end
@@ -88,7 +103,7 @@ sol.Kl = Kl;
 
 end
 
-function Vdot = valueFunction(t,State,ocp,x)
+function Vdot = valueFunction(t,State,ocp,x,u)
 
 len = ocp.dimension;
 
@@ -98,32 +113,38 @@ Vx = State(1+(1:len.state));
 % Vl = State(1+len.state+(1:len.adjoint));
 Vxx = reshape(State(1+len.state+len.adjoint+(1:len.state^2)),len.state,len.state);
 % Vll = reshape(State(1+len.state+len.adjoint+len.state^2+(1:len.adjoint^2)),len.adjoint,len.adjoint);
-Vxl = reshape(State(1+len.state+len.adjoint+len.state^2+len.adjoint^2:end),len.state,len.adjoint);
+Vxl = reshape(State(1+len.state+len.adjoint+len.state^2+len.adjoint^2+1:end),len.state,len.adjoint);
 
 %Compute the Jacobian and Hessian terms:
-F = Submatrix(ComplexDiff(ocp.dynamics,x(t)));
-JL = ComplexDiff(ocp.cost.lagrange,x(t));
-L = Submatrix(JL);
-HL = Hessian(ocp.cost.lagrange,x(t)); %Hessian of the lagrange cost
-H = Submatrix(HL); %+ order*(SOMEHESSIANCRAP); %Only 0th order for now
+F = Submatrix(ComplexDiff(@(X)ocp.dynamics(t,X(ocp.ind.state),X(ocp.ind.control)),[x(t)';u(t)]),ocp.dimension,ocp.ind);
+JL = ComplexDiff(@(X)ocp.cost.lagrange(X(ocp.ind.state),X(ocp.ind.control)),[x(t)';u(t)]);
+% L = Submatrix(JL,ocp.dimension,ocp.ind);
+L.x = JL(ocp.ind.state);
+L.u = JL(ocp.ind.control);
+HL = Hessian(@(X)ocp.cost.lagrange(X(ocp.ind.state),X(ocp.ind.control)),[x(t)';u(t)]); %Hessian of the lagrange cost
+% H = Submatrix(HL); %+ ocp.order*(SOMEHESSIANCRAP); %Only 0th order for now
+H.xx = HL(ocp.ind.state,ocp.ind.state);
+H.xu = HL(ocp.ind.state,ocp.ind.control);
+H.ux = H.xu';
+H.uu = HL(ocp.ind.control,ocp.ind.control);
 
 %Compute the Gain terms:
 Huui = inv(H.uu);
-l = -Huui*(L.u+F.u*Vx);
-Kx = -Huui*(0.5*H.ux + 0.5*H.xu' + F.u*Vxx); %This can be simplified
-Kl = -Huui*Fu*Vxl;
+l = -Huui*(L.u+F.control'*Vx);
+Kx = -Huui*(0.5*H.ux + 0.5*H.xu' + F.control'*Vxx); %This can be simplified
+Kl = -Huui*F.control'*Vxl;
 
 %The value function itself:
-dV = 0.5*l'*H.uu*l - L.matrix;
+dV = 0.5*l'*H.uu*l - ocp.cost.lagrange(x(t)',u(t));
 %First partial derivatives:
-dVx = -L.x - F.x*Vx + Kx'*H.uu*l;
+dVx = -L.x - (F.state*Vx + Kx'*H.uu*l)';
 dVl = -Kl'*L.u;
 %Second partial derivatives:
-dVxx = -H.xx + Kx'*H.uu*Kx - Vxx*F.x' - F.x*Vxx;
+dVxx = -H.xx + Kx'*H.uu*Kx - Vxx*F.state' - F.state*Vxx;
 dVll = Kl'*H.uu*Kl;
-dVxl = -H.xu*Kl - Fx*Vxl - Vxx*Fu'*Kl;
+dVxl = -H.xu*Kl - F.state*Vxl - Vxx*F.control*Kl;
 
-Vdot = [dV;dVx;dVl;reshape(dVxx,[],1);reshape(dVll,[],1);reshape(dVxl,[],1)];
+Vdot = [dV;dVx(:);dVl(:);reshape(dVxx,[],1);reshape(dVll,[],1);reshape(dVxl,[],1)];
 
 end
 
@@ -145,6 +166,21 @@ Kl = -Huui*Fu*Vxl;
 
 end
 
+function [H,L,F] = computePartials(ocp)
+
+F = Submatrix(ComplexDiff(@(X)ocp.dynamics(t,X(ocp.ind.state),X(ocp.ind.control)),[x(t)';u(t)]),ocp.dimension,ocp.ind);
+JL = ComplexDiff(@(X)ocp.cost.lagrange(X(ocp.ind.state),X(ocp.ind.control)),[x(t)';u(t)]);
+% L = Submatrix(JL,ocp.dimension,ocp.ind);
+L.x = JL(ocp.ind.state);
+L.u = JL(ocp.ind.control);
+HL = Hessian(@(X)ocp.cost.lagrange(X(ocp.ind.state),X(ocp.ind.control)),[x(t)';u(t)]); %Hessian of the lagrange cost
+% H = Submatrix(HL); %+ order*(SOMEHESSIANCRAP); %Only 0th order for now
+H.xx = HL(ocp.ind.state,ocp.ind.state);
+H.xu = HL(ocp.ind.state,ocp.ind.control);
+H.ux = H.xu';
+H.uu = HL(ocp.ind.control,ocp.ind.control);
+end
+
 function [V,Vx,Vl,Vxx,Vll,Vxl] = parseValueStates(VState,ocp)
 len = ocp.dimension;
 for i = 1:size(VState,1)
@@ -154,6 +190,6 @@ for i = 1:size(VState,1)
     Vl{i} = State(1+len.state+(1:len.adjoint));
     Vxx{i} = reshape(State(1+len.state+len.adjoint+(1:len.state^2)),len.state,len.state);
     Vll{i} = reshape(State(1+len.state+len.adjoint+len.state^2+(1:len.adjoint^2)),len.adjoint,len.adjoint);
-    Vxl{i} = reshape(State(1+len.state+len.adjoint+len.state^2+len.adjoint^2:end),len.state,len.adjoint);
+    Vxl{i} = reshape(State(1+len.state+len.adjoint+len.state^2+len.adjoint^2+1:end),len.state,len.adjoint);
 end
 end
