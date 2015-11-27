9f07096a90dbdbe91d316f10eef2d6878a41d2d8
Connor Noyes <aero.noyes@gmail.com>
2015-11-26 21:56:01 -0800

    Updated the inverted pendulum definition to match what is in the CTDDP paper.

--- Matlab/DynamicalSystems/InvertedPendulum.m
+++ Matlab/DynamicalSystems/InvertedPendulum.m
@@ -1,12 +1,19 @@
 function [fIP,jIP] = InvertedPendulum()
 
-    fIP = @(t,x,u) [x(2); ControlWrapper(t,u)-4*sin(x(1))];
+    m = 1; %kg
+    l = 0.5; %m
+    b = 1;
+    I = m*l^2;
+    g = 9.81;
+
+    fIP = @(t,x,u) [x(2); (ControlWrapper(t,u) + m*g*l*sin(x(1)) - b*x(2))/I];
     jIP = @InvertedPendulumJacobian;
 end
 
-function dfdx = InvertedPendulumJacobian(x,u)
+function df = InvertedPendulumJacobian(x,u)
 
-dfdx = [0, 1;-4*cos(x(1)) 0];
-% dfdu = [0;1];
+dfdx = [0, 1; m*g*l*cos(x(1)), -b/I];
+dfdu = [0;1/I];
+df = [dfdx, dfdu];
 
 end
