901e3436c31311f35e5ee1a555d9d60a6957855d
Connor Noyes <aero.noyes@gmail.com>
2015-11-27 14:04:09 -0800

    Added a test script for CTDDP.

---
+++ Matlab/TestScripts/testCTDDP.m
@@ -0,0 +1,32 @@
+%test the CTDDP algorithm on some dynamical systems:
+
+%% Inverted pendulum problem from the CTDDP paper for comparison
+clear; clc;
+
+[fIP,jIP] = InvertedPendulum(); %Get the dynamics and jacobian
+x0 = [pi,;0];
+xf = [0;0];
+%Cost functions
+Qf = diag([100,10]);
+R = 0.1;
+lagrange = @(x,u) u.'*R*u;
+mayer = @(t,x) x.'*Qf*x;
+constraints = [];
+hess.lagrange = 2*R;
+hess.mayer = 2*Qf;
+hess.dynamics = [];
+
+%Set the bounds
+bounds.upper.finalTime = 0.5;
+bounds.lower.finalTime = 0.5;
+bounds.upper.initialState = x0;
+bounds.lower.initialState = x0;
+bounds.upper.finalState = xf; 
+bounds.lower.finalState = xf;
+bounds.lower.control = -inf;
+bounds.upper.control = inf;
+OCP = OptimalControlProblem(fIP, lagrange, mayer, constraints, bounds, hess);
+OCP.dimension.adjoint = 0; %This means we will treat the final conditions as a soft constraint
+
+%Call the solver
+sol = CTDDP(OCP,0);
