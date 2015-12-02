7ecf86558a6227eec2ce832fefc7d32902b84669
Connor Noyes <aero.noyes@gmail.com>
2015-12-02 15:42:38 -0800

    Added analytical hessian to InvertedPendulum.

--- Matlab/DynamicalSystems/InvertedPendulum.m
+++ Matlab/DynamicalSystems/InvertedPendulum.m
@@ -1,4 +1,4 @@
-function [fIP,jIP] = InvertedPendulum()
+function [fIP,jIP,hIP] = InvertedPendulum()
 
     m = 1; %kg
     l = 0.5; %m
@@ -8,12 +8,23 @@ function [fIP,jIP] = InvertedPendulum()
 
     fIP = @(t,x,u) [x(2); (ControlWrapper(t,u) + m*g*l*sin(x(1)) - b*x(2))/I];
     jIP = @(x,u) InvertedPendulumJacobian(x,u,m,l,g,I,b);
+    hIP = @(x,u) InvertedPendulumHessian(x,u,m,l,g,I,b);
+
 end
 
 function df = InvertedPendulumJacobian(x,u,m,l,g,I,b)
 
-dfdx = [0, 1; m*g*l*cos(x(1)), -b/I];
+dfdx = [0, 1; m*g*l*cos(x(1))/I, -b/I];
 dfdu = [0;1/I];
 df = [dfdx, dfdu];
 
+end
+
+
+function H = InvertedPendulumHessian(x,u,m,l,g,I,b)
+
+d2fdx2{2} = [-m*g*l*sin(x(1))/I, 0, 0;0,0,0;0,0,0];
+d2fdx2{1} = zeros(3);
+
+H = sparse(blkdiag(d2fdx2{:}));
 end
