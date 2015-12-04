93814f908754b115a8853a7ada935760bbd1ba96
Connor Noyes <CDNoyes@users.noreply.github.com>
2015-12-03 19:28:24 -0800

    Added an example using a scalar function of multiple variables to the submatrix tests.

--- Matlab/TestScripts/testSubmatrix.m
+++ Matlab/TestScripts/testSubmatrix.m
@@ -1,6 +1,6 @@
 %test out Submatrix on Jacobians and Hessians
 
-%VDP Oscillator
+%% VDP Oscillator
 clear; clc;
 [F,J,H] = VDP();
 [dim,ind] = Dimension(2,1,1);
@@ -20,3 +20,16 @@ hess = Submatrix(h,dim,ind);
 % hess.xx == hxx?
 fx = @(X) F(0,X,u0,mu);
 hxx = Hessian(fx,x0);
+
+%% Test the scalar functionality
+%Crazy made-up function of 3 states, 2 controls, and 1 parameter
+clc; clear;
+f = @(x,u,p) sum(x)*x(1)*u(1)/(u(2)+p^2);
+[dim,ind] = Dimension(3,2,1);
+
+F = @(X) f(X(ind.state),X(ind.control),X(ind.parameter));
+X = [1;1;1;1;1;-1];
+J = ComplexDiff(F,X);
+H = Hessian(F,X);
+j = Submatrix(J,dim,ind,true);
+h = Submatrix(H,dim,ind,true);
