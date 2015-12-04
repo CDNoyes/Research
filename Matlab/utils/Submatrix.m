003a10f901476412b54ee07fe5f0661eb2901443
Connor Noyes <CDNoyes@users.noreply.github.com>
2015-12-03 19:26:51 -0800

    Submatrix now uses the isScalar flag and can be used on jacobians/hessians of either system dynamics or scalar functions.

--- Matlab/utils/Submatrix.m
+++ Matlab/utils/Submatrix.m
@@ -1,10 +1,10 @@
 %Implementation that deduces whether the matrix M is a Jacobian or Hessian
-%matrix, whether ind is refering to the indices of the matrix or a
-%structure for the indices of state,control, and parameter.
+%matrix. dim and ind are structures with information about state,control,
+%and parameter created using the Dimension function.
 
 function output = Submatrix(M,dim,ind,isScalar)
-if ~isstruct(ind)
-    error('Ind must be a struct input, as from Dimension.m')
+if ~isstruct(ind) || ~isstruct(dim)
+    error('Dim and Ind must be struct inputs, as from Dimension.m')
 end
 if nargin < 4 || isempty(isScalar)
     isScalar = false;
@@ -12,20 +12,18 @@ end
 d = size(M);
 
 % isTensor = length(d) == 3; %We never use hessian tensors now
-isHessianCell = isa(M,'cell') ;%Like the second output of Hessian.m
-isHessianMatrix = d(1) ~= dim.state; %Like the first output of Hessian.m
-
+% isHessianCell = isa(M,'cell') ;%Like the second output of Hessian.m
+if isScalar
+    loopInd = 1;
+    isHessianMatrix = d(1) == d(2); %Hessian will be square, Jacobian a vector
+else
+    loopInd = dim.state:-1:1;
+    isHessianMatrix = d(1) ~= dim.state; %Like the first output of Hessian.m
+end
 output.matrix = M;
 
-if isHessianCell
-    %     output.xx
-    %     output.xu
-    %     output.xp;
-    %     output.uu;
-    %     output.pp;
-    %     output.up;
-elseif isHessianMatrix
-    for i = dim.state:-1:1
+if isHessianMatrix
+    for i = loopInd
         hxx{i} = M((i-1)*(dim.total)+ind.state,(i-1)*(dim.total)+ind.state);
         hxu{i} = M((i-1)*(dim.total)+ind.state,(i-1)*(dim.total)+ind.control);
         hxp{i} = M((i-1)*(dim.total)+ind.state,(i-1)*(dim.total)+ind.parameter);
