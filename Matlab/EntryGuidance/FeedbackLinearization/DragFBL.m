3233093e0b39d4764bc7bd63f8bfd1d675939098
Connor Noyes <aero.noyes@gmail.com>
2016-02-13 19:29:51 -0800

    Fixed a small error in DragFBL

--- Matlab/EntryGuidance/FeedbackLinearization/DragFBL.m
+++ Matlab/EntryGuidance/FeedbackLinearization/DragFBL.m
@@ -1,20 +1,17 @@
+% DRAGFBL Returns the parameters a,b such that the second time rate of
+% change of drag = a+bu.
+%   Even when using a control law not based on FBL, this function is used
+%   in the sliding mode observer
+
 function [a,b] = DragFBL(g,L,D,r,V,gamma,rho,rho_dot,D_dot)
-
-if isempty(D_dot)
-    D_dot = -D*V*sin(gamma)/hs - 2*D/V*(D+g*sin(gamma));
-end
-% 
-% a = -D_dot*V*sin(gamma)/hs + D*(D+g*sin(gamma))*sin(gamma)/hs + ...
-%     D*cos(gamma)^2*(g-V^2/r)/hs - 2*D_dot*(D+g*sin(gamma))/V^2 + ... %the second term has a minus
-%     -2*D/V^2*(D+g*sin(gamma))^2 - 2*D*D_dot/V + 4*D*g*sin(gamma)^2/r + ...
-%     2*D/V^2*g*cos(gamma)^2*(g-V^2/r);
-% 
-% b = -D*L*cos(gamma)*(2*g/V^2 +(1/hs));
-
 
 V_dot = -D-g*sin(gamma);
 g_dot = -2*g*V*sin(gamma)/r;
 h_dot = V*sin(gamma);
+
+if isempty(D_dot) %When using an observer, we used the observed estimate instead of the model estimate
+    D_dot = D*(rho_dot/rho + 2*V_dot/V); %+ D*CD_dot/CD %neglect the variation in C_D
+end
 
 a1 = D_dot*(rho_dot/rho + 2*V_dot/V) - D*(rho_dot^2/rho^2 + 2*V_dot^2/V^2);
 a2 = -2*D/V*(D_dot+g_dot*sin(gamma));
