4307e7f588f2d981a0309f3f117b85f4fec4495b
Connor Noyes <aero.noyes@gmail.com>
2016-02-13 19:17:48 -0800

    Updated the SMO function's help

--- Matlab/EntryGuidance/FeedbackLinearization/SMO.m
+++ Matlab/EntryGuidance/FeedbackLinearization/SMO.m
@@ -1,15 +1,18 @@
-%Sliding mode observer for drag tracking
-% x is the vector of observer states
-% D is the measured drag acceleration
-% a and b are the FBL terms for drag tracking
-% k is a vector of linear gains
-% alpha is a vector of nonlinear gains
+% SMO Sliding mode observer dynamics for drag tracking.
+%	SMO(x,D,a,b,u,k,alpha) Computes the change in estimates of drag, drag
+%	rate, and disturbance. The inputs are:
+%     x - the vector of observer states
+%     D - the measured drag acceleration
+%     a,b - FBL terms for drag tracking
+%     u - cos(gamma), the control parameter
+%     k - vector of linear gains
+%     alpha - vector of nonlinear gains
 
 function dx = SMO(x,D,a,b,u,k,alpha)
 
 e = D-x(1);
 % signe = sign(e);
-signe = tanh(10*e); %smooth approximation to sign function
+signe = tanh(100*e); %smooth approximation to sign function
 dx = [ x(2) + alpha(1)*e + k(1)*signe
        x(3) + alpha(2)*e + k(2)*signe + a + b*u
               alpha(3)*e + k(3)*signe ];
