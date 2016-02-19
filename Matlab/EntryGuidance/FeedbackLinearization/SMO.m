62b4998b00af54025f8242c9184f18542f6f9107
Connor Noyes <CDNoyes@users.noreply.github.com>
2016-02-19 13:56:33 -0800

    Changed the help text and lowered the tanh approximation a bit in the SMO design.

--- Matlab/EntryGuidance/FeedbackLinearization/SMO.m
+++ Matlab/EntryGuidance/FeedbackLinearization/SMO.m
@@ -3,7 +3,7 @@
 %	rate, and disturbance. The inputs are:
 %     x - the vector of observer states
 %     D - the measured drag acceleration
-%     a,b - FBL terms for drag tracking
+%     a,b - Drag dynamics, D_ddot = a(x) + b(x)u
 %     u - cos(gamma), the control parameter
 %     k - vector of linear gains
 %     alpha - vector of nonlinear gains
@@ -12,7 +12,7 @@ function dx = SMO(x,D,a,b,u,k,alpha)
 
 e = D-x(1);
 % signe = sign(e);
-signe = tanh(100*e); %smooth approximation to sign function
+signe = tanh(10*e); %smooth approximation to sign function
 dx = [ x(2) + alpha(1)*e + k(1)*signe
        x(3) + alpha(2)*e + k(2)*signe + a + b*u
               alpha(3)*e + k(3)*signe ];
