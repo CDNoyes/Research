9b0824c8217a3764790e52ee72d3466ce74e74a2
Connor Noyes <aero.noyes@gmail.com>
2016-03-07 14:26:42 -0800

    Changed the parameters a bit, redesigned the sliding mode.

--- Matlab/DynamicalSystems/TimeVaryingSMC.m
+++ Matlab/DynamicalSystems/TimeVaryingSMC.m
@@ -1,15 +1,18 @@
 function TimeVaryingSMC()
 
 x0 = [0.5;0.4];
-e0 = x0(1);
-limit = 8;
-margin = limit-6;
-[T,A,B] = DesignSM(e0,margin);
-
-[t,x] = ode45(@dynamics,[0,100],x0,[],A,B,T);
+e0 = [x0(1),x0(2)-0.4];
+[t,x] = ode45(@dynamics,[0,40],x0,[],e0);
 for i = 1:length(t)
-    [~,u(i),s(i),d(i)] = dynamics(t(i),x(i,:)',A,B,T);
+    [~,u(i),s(i),d(i)] = dynamics(t(i),x(i,:)',e0);
 end
+iT = find(t>1,1);
+
+figure
+plot(x(:,1)-2*sin(0.2*t),x(:,2)-0.2*2*cos(0.2*t))
+hold on
+plot(x(iT,1)-2*sin(0.2*t(iT)),x(iT,2)-0.2*2*cos(0.2*t(iT)),'ro')
+title('Error trajectory')
 
 figure
 plot(t,[2*sin(0.2*t),0.2*2*cos(0.2*t)])
@@ -25,17 +28,15 @@ ylabel('u')
 figure
 plot(t,s)
 title('Sliding Surface')
+axis([0,t(end),-1,1])
 end
 
-function [dx,u,s,d] = dynamics(t,x,A,B,T)
+function [dx,u,s,d] = dynamics(t,x,e0)
+
 
 %System format
 f = -2*x(1)+3*(1-x(1)^2)*x(2);
 b = 1;
-
-% Sliding Surface Design Parameters
-beta = 2;
-c = [beta,1];
 
 % Reference Trajectory
 yd = 2*sin(0.2*t);
@@ -50,12 +51,19 @@ dmax = 5; %Estimated bound of the disturbance
 % Error states
 e = x-[yd;yd_dot];
 
+% Sliding Surface Design Parameters
+beta = 1;
+T = 1; %The time at which the sliding surface stops moving
+B = -beta*e0(1)-e0(2);
+A = -B/T;
+c = [beta,1];
+
 %Sliding Surface Computation
 s = c*e + (A*t+B)*(t<=T); 
 
 % Control
 u0 = yd_ddot-f - dmax*Saturate(s/.01,-1,1);
-u = 1/b*u0 + 1/b*(-beta*e(2)-A*(t<=T));
+u = Saturate(1/b*u0 + 1/b*(-beta*e(2)-A*(t<=T)),-10,10);
 
 dx = [  x(2)
         f + b*u + d];
