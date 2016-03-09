6b187293e738776a5ca5092e6214426cf450e509
Connor Noyes <aero.noyes@gmail.com>
2016-03-09 13:56:33 -0800

    Restructured the time vary smc code a bit, added more output plots, added an observer for state/perturbation to be used in the controller.

--- Matlab/DynamicalSystems/DesignSM.m
+++ Matlab/DynamicalSystems/DesignSM.m
@@ -12,19 +12,55 @@
 %
 % Reference: "Time Varying Sliding Modes for Second Order Systems"
 
-function [T,A,B,C,beta] = DesignSM(e0, Umax)
+function [sig,U,T] = DesignSM(e0, Umax, type)
+
+if e0 == 0
+    sig = @(e,t) 0;
+    U = @(b,u0,e,t) 1/b*u0;
+    T = 0;
+    return
+end
 
 se0 = sign(e0);
 
 
-%Constant Velocity:
-T = sqrt(2*e0/Umax);
-A = Umax*se0;
-B = -se0*sqrt(2*Umax*abs(e0));
-C = [];
-beta = sqrt(2*Umax/abs(e0));
+if strcmpi(type,'Velocity')     % Constant Velocity:
+    T = sqrt(2*e0/Umax);
+    A = Umax*se0;
+    B = -se0*sqrt(2*Umax*abs(e0));
+    beta = sqrt(2*Umax/abs(e0));
+    sig = @(e,t) [beta,1]*e + (A*t+B)*(t<=T); 
+    U = @(b,u0,e,t) 1/b*u0 + 1/b*(-beta*e(2)-A*(t<=T));
+    
+else                            % Terminal
+    T = sqrt(6*abs(e0)/Umax);
+    A = -0.5*se0*Umax;
+    B = e0*sqrt(6*Umax/abs(e0));
+    C = -3*e0;
+    alpha = abs(fixPower(e0))*sqrt(6*Umax/abs(e0))/(4^(1/3));
+    
+    sig = @(e,t) SMT(T,A,B,C,alpha,e,t);
+    U = @(b,u0,e,t) controllerT(T,A,B,C,alpha,b,u0,e,t);
+end
 
+end
 
+function s = SMT(T,A,B,C,alpha,e,t)
 
+% s = e(2) + real(2*A*t+B+alpha*Saturate((e(1)+A*t^2+B*t+C)/.01,-1,1)*(e(1)+A*t^2+B*t+C)^(2/3))*(t<=T) + real(alpha*Saturate(e(1)/.01,-1,1)*e(1)^(2/3))*(t>T);
+s = e(2) + (2*A*t+B+alpha*Saturate((e(1)+A*t^2+B*t+C)/.01,-1,1)*fixPower(e(1)+A*t^2+B*t+C)^2)*(t<=T) + (alpha*Saturate(e(1)/.01,-1,1)*fixPower(e(1))^2)*(t>T);
+
+end
+
+function u = controllerT(T,A,B,C,alpha,b,u0,e,t)
+
+% u = 1/b*u0 + 1/b*( real(-2*A+2/3*alpha^2*(e(1)+A*t^2+B*t+C)^(1/3))*(t<=T) + real(2/3*(t>T)*alpha^2*e(1)^(1/3)) );
+u = u0/b + 1/b*( (-2*A+2/3*alpha^2*fixPower(e(1)+A*t^2+B*t+C))*(t<=T) + (2/3*(t>T)*alpha^2*fixPower(e(1))) );
+
+end
+
+function n = fixPower(m)
+
+n = sign(m)*abs(m)^(1/3);
 
 end
--- Matlab/DynamicalSystems/TimeVaryingSMC.m
+++ Matlab/DynamicalSystems/TimeVaryingSMC.m
@@ -1,26 +1,41 @@
+% Reference: "Time Varying Sliding Modes for Second Order Systems"
+
+
 function TimeVaryingSMC()
 
-x0 = [0.5;0.4];
+%Initial States
+x0 = [0.9;0.4];
+x0_hat = [0.1;0.4];
 e0 = [x0(1),x0(2)-0.4];
-[t,x] = ode45(@dynamics,[0,100],x0,[],e0);
+d0 = 0.6;
+
+%Design controller, and observer
+% Sliding Surface Design Parameters
+% [S,controller,T] = DesignSM(e0(1),1,'Terminal');
+[S,controller,T] = DesignSM(e0(1),1,'Velocity');
+[alpha, k] = computeSMOGains(400);
+
+%Integrate the controlled trajectory
+[t,x] = ode45(@dynamics,[0,100],[x0;x0_hat;d0],[],S,controller,k,alpha);
 for i = 1:length(t)
-    [~,u(i),s(i),d(i),T] = dynamics(t(i),x(i,:)',e0);
+    [~,u(i),s(i),d(i),d_hat(i)] = dynamics(t(i),x(i,:)',S,controller,k,alpha);
 end
 iT = find(t>T,1);
 
+%Plot the results
 figure
 plot(x(:,1)-2*sin(0.2*t),x(:,2)-0.2*2*cos(0.2*t))
 hold on
 plot(x(iT,1)-2*sin(0.2*t(iT)),x(iT,2)-0.2*2*cos(0.2*t(iT)),'ro')
 plot(0,0,'kx',e0(1),e0(2),'ko')
-legend('Trajectory',['Time invariance (t=',num2str(t(iT)),' s)'],'Target','IC')
+legend('Trajectory',['Time Invariance (t = ',num2str(T),' s)'],'Target','IC')
 title('Error trajectory')
 
 figure
 plot(t,[2*sin(0.2*t),0.2*2*cos(0.2*t)])
 hold all
-plot(t,d)
-plot(t,x)
+plot(t,d,'--')
+plot(t,x(:,1:2))
 legend('Ref','Ref rate','Disturbance')
 
 figure
@@ -32,10 +47,26 @@ figure
 plot(t,s)
 title('Sliding Surface')
 axis([0,t(end),-1,1])
+
+% figure
+% plot(t,d)
+% hold all
+% plot(t,d_hat,t,x(:,6))
+% title('Disturbance Estimate History')
+
+figure
+plot(t,x(:,1:2)-x(:,3:4))
+hold all
+plot(t,d-d_hat)
+title('Estimation Error History')
+legend('x_1','x_2','d')
 end
 
-function [dx,u,s,d,T] = dynamics(t,x,e0)
-
+function [dX,u,s,d,d_hat] = dynamics(t,X,S,controller,k,alpha)
+%Parse the extended state vector
+x = X(1:2);
+x_hat = X(3:4);
+d_hat = X(5);
 
 %System format
 f = -2*x(1)+3*(1-x(1)^2)*x(2);
@@ -49,27 +80,41 @@ yd_ddot = -0.2^2*2*sin(0.2*t);
 % Disturbance
 d = 2*sin(0.1*pi*t)+3*sin(0.2*sqrt(t+1));
 % d = 0;
-dmax = d; %Estimated bound of the disturbance
+dmax = 5*(t<.1) + abs(d_hat)*(t>=.1); % Estimated bound of the disturbance
 
 % Error states
 e = x-[yd;yd_dot];
 
-% Sliding Surface Design Parameters
-[T,A,B,~,beta] = DesignSM(e0(1),8.8);
-if t == 0
-    disp(beta)
-end
-c = [beta,1];
-
 %Sliding Surface Computation
-s = c*e + (A*t+B)*(t<=T); 
+s = S(e,t);
 
 % Control
 u0 = yd_ddot-f - dmax*Saturate(s/.01,-1,1);
+u = controller(b,u0,e,t);
 U = 10;
-u = Saturate(1/b*u0 + 1/b*(-beta*e(2)-A*(t<=T)),-U,U);
+u = Saturate(u,-U,U);
+
 
 dx = [  x(2)
-        f + b*u + d];
-    
+        f + b*u + d]; 
+
+    % This is the disturbance observer term
+dx_hat = SMO([x_hat;d_hat],x(1),f,b,u,k,alpha);
+
+dX = [dx;dx_hat];
+
+end
+
+function [alpha, k] = computeSMOGains(lambda)
+% -lambda is where the three observer poles are placed
+
+% M = [-2,1;-1,0];
+% P1 = lyap(M,eye(2)); = [0.5 0.5; 0.5 1.5]
+% sqrt(cond(P1)) = 2.4142 so k1 needs to be 2.5 times greater than the
+% initial x_1 observation error
+C3 = (factorial(3)./(factorial(1:3).*factorial(3-(1:3))));
+alpha = C3.*lambda.^(1:3);
+k(1) = 5;
+k(2:3) = k(1)*[2,1].*lambda.^(1:2);
+% k = [2.5852,14.2055,19.5150];
 end
