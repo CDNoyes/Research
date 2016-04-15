2c341a13ef232b4c24e6431e2c874359207f0d09
Connor Noyes <aero.noyes@gmail.com>
2016-04-15 11:08:32 -0700

    Still trying various things with the lateral guidance commands.

--- Matlab/EntryGuidance/LateralGuidance.m
+++ Matlab/EntryGuidance/LateralGuidance.m
@@ -1,16 +1,25 @@
 %Reference: "Drag-Based Predictive Tracking Guidance for Mars Precision
 %Landing"
 
-function [s,headingErrorLimit] = LateralGuidance(CR,V,currentsign)
+function [s,headingErrorLimit] = LateralGuidance(CR,IV,currentsign)
 
-lim0 = 3;
-limf = 0.25;
-v0 = 5505;
-vf = 2000;
-M = (lim0-limf)/(v0-vf);
-CR0 = lim0-M*v0;
 
-headingErrorLimit = limf*(V<vf) + (CR0 + V*M).*(V>=vf);
+% Velocity Based Profile:
+% V = IV;
+% lim0 = 3;
+% limf = 0.25;
+% v0 = 5505;
+% vf = 2000;
+% M = (lim0-limf)/(v0-vf);
+% CR0 = lim0-M*v0;
+% 
+% headingErrorLimit = limf*(V<vf) + (CR0 + V*M).*(V>=vf);
+
+% Drag Based Profile:
+D = IV;
+[cr_coeff,s] = polyfit([0,80.62, 7.416, 120],[0,5.05-1, 0.8, 6-1],3);
+headingErrorLimit = polyval(cr_coeff,D);
+
 
 if abs(CR)>=headingErrorLimit && currentsign == sign(CR)
     s = -currentsign;
