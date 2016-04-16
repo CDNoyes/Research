2d6119dcdf4ebfa8ac9e8de398298daaf127826a
Connor Noyes <aero.noyes@gmail.com>
2016-04-16 16:14:12 -0700

    Changed the computation of traj length from meters to radians to be consistent with its intended use in PC guidance.

--- Matlab/EntryGuidance/TrajLength.m
+++ Matlab/EntryGuidance/TrajLength.m
@@ -1,10 +1,11 @@
+% TRAJLENGTH Computes the range (in radians) on the surface of a spherical
+% body along the great circle connecting the initial and final locations.
+
 function s = TrajLength(trajsum)
 time = trajsum.time;
 x = trajsum.state;
-sdot = @(t,s) interp1(time,x(:,4).*cos(x(:,5)),t);
+sdot = @(t,s) interp1(time,x(:,4).*cos(x(:,5))./x(:,1),t);
 
 [t,s] = ode45(sdot,time,0);
-
-
 
 end
