ffed6252e8b08f02cad97cf1d4109750cfb4943c
Connor Noyes <aero.noyes@gmail.com>
2016-04-26 21:33:10 -0700

    Added some help to quadratize and updated it to use the hessian multiply util.

--- Matlab/utils/Quadratize.m
+++ Matlab/utils/Quadratize.m
@@ -1,8 +1,14 @@
+% QUADRATIZE Computes a quadratic expansion.
+%   QUADRATIZE(F,X,J,H) Returns a function handle that gives the second
+%   order expansion of the function F around the point X. J and H are the
+%   jacobian and hessian of F. They are optional, if not given they will be
+%   estimated numerically.
+
 function quadFunction = Quadratize(fun,point,firstDerivative,secondDerivative)
 
 if nargin >= 3 && ~isempty(firstDerivative)
     df = firstDerivative;
-    if nargin ==4 && ~isempty(secondDerivative)
+    if nargin == 4 && ~isempty(secondDerivative)
         H = secondDerivative;
     else
         H = @(x) Hessian(fun,x);
@@ -13,15 +19,15 @@ else
 end
 f = fun(point);
 n = length(f);
-quadFunction = @(x) f+df(point)*(x-point)+0.5*hessianMultiply(H(point),x-point,n);
+quadFunction = @(x) f+df(point)*(x-point)+0.5*HessianMultiply(H(point),x-point,n);
 
 end
 
-function xHx = hessianMultiply(H,x,n)
-
-%X' = full(kron(sparse(eye(n)),x'));
-%X = repmat(X,n,1)
-
-xHx = full(kron(sparse(eye(n)),x'))*H*repmat(x,n,1);
-
-end
+% function xHx = hessianMultiply(H,x,n)
+% 
+% %X' = full(kron(sparse(eye(n)),x'));
+% %X = repmat(X,n,1)
+% 
+% xHx = full(kron(sparse(eye(n)),x'))*H*repmat(x,n,1);
+% 
+% end
