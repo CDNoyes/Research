46f6668d32afa3bff07df5bb233829d104ecc5cb
Connor Noyes <aero.noyes@gmail.com>
2016-07-17 11:41:48 -0700

    Small utility for plotting lines that change smoothly in color.

---
+++ Matlab/utils/ColorRange.m
@@ -0,0 +1,10 @@
+% COLORRANGE Returns a vector C with NPOINTS points to be use in plotting.
+% The range is optimized to change smoothly from COLOR1 to COLOR2
+
+function c = ColorRange(colorA,colorB,nPoints)
+
+for i=1:3
+c(1:nPoints,i) = linspace(colorA(i),colorB(i),nPoints);
+
+end
+end
