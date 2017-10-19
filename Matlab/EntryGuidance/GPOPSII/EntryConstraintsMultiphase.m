729860ac7cb2b04c1245e81dbef1a5a02c769f40
Connor Noyes <cdnoyes@jpl.nasa.gov>
2017-10-19 16:19:56 -0700

    Adds some (possibly non functional) multiphase cost and constraints for using the HEP in GPOPS

---
+++ Matlab/EntryGuidance/GPOPSII/EntryConstraintsMultiphase.m
@@ -0,0 +1,46 @@
+function output = EntryConstraintsMultiPhase( input )
+
+S       = input.auxdata.vehicle.area;
+m       = input.auxdata.vehicle.mass;
+mu      = input.auxdata.planet.mu;
+rp      = input.auxdata.planet.radiusEquatorial;
+d       = input.auxdata.delta;
+
+bankmin=0;
+bankmax=90*pi/180;
+bank_angles = [bankmin,-bankmax,bankmax,-bankmin];
+
+for iphase = 1:input.auxdata.nPhases
+    x = input.phase(iphase).state;
+    Sigma = bank_angles(iphase)*ones(size(x(:,1)));
+    
+    r = x(:,1);
+    theta = x(:,2);
+    phi = x(:,3);
+    v = x(:,4);
+    gamma = x(:,5);
+    psi = x(:,6);
+    
+    
+    g = mu./(r.^2);
+    h = r-rp;
+    [rho,a] = MarsAtmosphericDensity(h);
+    M = v./a;
+    [C_D,C_L] = AerodynamicCoefficients(M);
+    q = 0.5*rho.*v.^2*S/m;
+    drag = q.*(C_D+d.CD);
+    lift = q.*C_L;
+    
+    
+    dr = v.*sin(gamma);
+    dtheta = v.*cos(gamma).*cos(psi)./(r.*cos(phi));
+    dphi = v.*cos(gamma).*sin(psi)./r;
+    dv = -drag - g.*sin(gamma);
+    dgamma = lift.*cos(Sigma)./v - (g./v - v./r).*cos(gamma);
+    dpsi = -lift.*sin(Sigma)./(v.*cos(gamma)) - (v./r).*cos(gamma).*cos(psi).*tan(phi);
+    
+    output(iphase).dynamics = [dr dtheta dphi dv dgamma dpsi];
+    
+end
+
+end
---
+++ Matlab/EntryGuidance/GPOPSII/EntryCostMultiphase.m
@@ -0,0 +1,21 @@
+function output = EntryCostMultiphase( input )
+xf = input.phase(end).finalstate;
+p = input.parameter;
+
+% output.objective = -(xf(1)-input.auxdata.planet.radiusEquatorial)/1000 + 50*xf(5).^2;
+% output.objective = -xf(3); % Max cross range when starting on equator and heading east or west
+output.objective = -xf(2); % Max down range when starting on equator and heading east
+
+% Linkage constraints
+for i = 1:(input.auxdata.nPhases-1)
+    % Variables at Terminus of Phase i
+    tf1 = input.phase(i).finaltime;
+    xf1 = input.phase(i).finalstate;
+    % Variables at Start of Phase i+1
+    t02 = input.phase(i+1).initialtime;
+    x02 = input.phase(i+1).initialstate;
+    output.eventgroup(i).event = [x02-xf1, t02-tf1, p(i)-tf1];
+    
+end
+
+end
