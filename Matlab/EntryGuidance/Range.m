987fa0ae1c4a9fb650e4f716c56ec7ec4f0210bb
cdnoyes <aero.noyes@gmail.com>
2018-07-26 09:21:55 -0700

    Improved DR/CR calc from lat/lon

--- Matlab/EntryGuidance/Range.m
+++ Matlab/EntryGuidance/Range.m
@@ -1,24 +1,24 @@
 % RANGE Computes the downrange and crossrange of an entry vehicle given the
 % initial lat/long and heading, and the current lat/long. All inputs should
-% be in radians. The outputs are given in kilometers. The function is 
+% be in radians. The outputs are given in kilometers. The function is
 % currently hard coded for Mars; in the future, the planet radius ( or
 % planet structure) should be a required input as well.
-%
-% Note:
-% The output is slightly buggy when the current and initial coordinates are 
-% equal or nearly equal. That is why the real/isnan fixes are in place. The
-% equations are undefined at this point due to a divide by zero error.
 
-function [DR,CR] = Range(theta0,phi0,psi0,theta,phi)
+function [DR,CR] = Range(lon0,lat0,heading0,lonc,latc)
 
 r_p = 3397; %km
-LF = real(acos(sin(phi)*sin(phi0)+cos(phi).*cos(phi0).*cos(theta-theta0)));
-sig = asin(sin(theta-theta0).*cos(phi)./sin(LF));
-zeta = sig+psi0-pi/2;
 
-DR = real(r_p*acos(cos(LF)./cos(asin(sin(LF).*sin(zeta)))));
-CR = -real(r_p*asin(sin(LF).*sin(zeta)));
-DR(isnan(DR)) = 0;
-CR(isnan(CR)) = 0;
-
+d13 = acos(sin(latc).*sin(lat0)+cos(latc).*cos(lat0).*cos(lonc-lon0));
+% if abs(d13) < 1e-4
+%     DR=0.*d13;
+%     CR=0.*d13;
+% else
+    psi12 = heading0;
+    PHI = sign(lonc-lon0).*acos( (sin(latc) - sin(lat0).*cos(d13))./(cos(lat0).*sin(d13)) );
+    psi13 = pi/2 - PHI;
+    CR = asin(sin(d13).*sin(psi12-psi13));
+    DR = r_p*acos(cos(d13)./cos(CR));
+    CR = -r_p*CR;
+    
+% end
 end
