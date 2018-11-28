447530340871ed26f5f834691c0b53a59293ef88
cdnoyes <aero.noyes@gmail.com>
2018-11-27 20:55:31 -0800

    Changes to cov min

--- Matlab/EntryGuidance/GPOPSII/covariance_min.m
+++ Matlab/EntryGuidance/GPOPSII/covariance_min.m
@@ -1,18 +1,22 @@
 function output = covariance_min()
-% Computes the optimal solution to covariance minimization of scalar
-% nonlinear system 
-if 0
+% Computes the optimal solution to variance minimization of scalar
+% nonlinear system
+if 1
     close all;
-   
+    
 end
 clc;
+
+global P0 CLOSED_LOOP
+P0 = 0.0069; % 0.0069= +/-0.25 3-sigma
+CLOSED_LOOP = true;
 
 output = SolveOCP();
 
 sol = output.result.solution.phase(1);
 STM = sol.state(:,2);
-P0 = 1;
 P = STM.^2 * P0;
+std = sqrt(P);
 
 D = Dynamics(output.result.solution);
 dx = D.dynamics(:,1);
@@ -26,8 +30,11 @@ H = sol.costate(:,1).*dx + sol.costate(:,2).*dstm;
 % Plots
 figure(1)
 hold all
-plot(sol.time,sol.state)
-legend('x', 'stm')
+plot(sol.time,sol.state(:,1))
+plot(sol.time,sol.state(:,1)+3*std,'k--')
+plot(sol.time,sol.state(:,1)-3*std,'k--')
+
+legend('x', '\pm 3\sigma')
 title('States')
 set(gcf,'name','Position vs Time','numbertitle','off')
 set(gcf,'WindowStyle','docked')
@@ -63,15 +70,24 @@ plot(sol.time, H)
 title('Hamiltonian')
 set(gcf,'name','H vs Time','numbertitle','off')
 set(gcf,'WindowStyle','docked')
-
+if CLOSED_LOOP
+    figure(6)
+    hold all
+    plot(sol.time,D.gains)
+    title('Gain')
+    set(gcf,'name','Gain vs Time','numbertitle','off')
+    set(gcf,'WindowStyle','docked')
+end
 output = output.result.solution;
 
 end
 
 function output = SolveOCP()
+global CLOSED_LOOP
+
 t0 = 0;
-tmin = 0.1;
-tmax = 2;
+tmin = 1;
+tmax = 1;
 
 x0 = 1;
 stm0 = 1;
@@ -79,7 +95,7 @@ stm0 = 1;
 xmin = -10;
 xmax = 10;
 
-xf = 0; 
+xf = 0;
 umax =  3;
 
 %% Bounds Definition
@@ -101,21 +117,21 @@ bounds.phase(iphase).finalstate.upper          = [xf 1000];
 
 bounds.phase(iphase).control.lower = [-umax];
 bounds.phase(iphase).control.upper = [umax];
-
-% bounds.phase(iphase).path.lower = 1;
-% bounds.phase(iphase).path.upper = 1;
-% 
+if CLOSED_LOOP
+    bounds.phase(iphase).path.lower = [-umax, -umax];
+    bounds.phase(iphase).path.upper = [umax, umax];
+end
 % bounds.phase(iphase).integral.lower = 0;
 % bounds.phase(iphase).integral.upper = tmax*amax^2;
 
 %% Guess
 guess.phase(iphase).time           = [0; tmax]; % 0.5*(tmin+tmax)
-guess.phase(iphase).state     = [x0, 1; xf, 0.1];
-guess.phase(iphase).control   = [-1; 1];
+guess.phase(iphase).state     = [x0, 1; xf, 1e-3];
+guess.phase(iphase).control   = [3; -3];
 % guess.phase(iphase).integral = [ 0.5*(amin+amax)*15];
 
 %% NLP Parameters and Optimization Call
-setup.name = 'SRP Optimization';
+setup.name = 'VarMin';
 setup.functions.continuous = @Dynamics;
 setup.functions.endpoint = @Cost;
 setup.nlp.solver = 'snopt';
@@ -134,8 +150,8 @@ setup.derivatives.derivativelevel = 'first'; % 'first' or 'second'
 setup.derivatives.dependencies = 'sparseNaN'; % 'full', 'sparse' or 'sparseNaN'
 setup.scales.method = 'automatic-guessUpdate'; % 'none' or 'automatic-bounds' or 'automatic-guess' or 'automatic-guessUpdate' or 'automatic-hybrid' or 'automatic-hybridUpdate' or 'defined'
 setup.mesh.method = 'hp-PattersonRao'; % 'hp-PattersonRao' or 'hp-DarbyRao' or 'hp-LiuRao'
-setup.mesh.tolerance = 1e-7; % Default 1e-3
-setup.mesh.maxiterations = 15; % Default 10
+setup.mesh.tolerance = 1e-5; % Default 1e-3
+setup.mesh.maxiterations = 30; % Default 10
 setup.method = 'RPM-Differentiation'; % 'RPM-Differentiation' or 'RPM-Integration'
 setup.displaylevel = 1; % 0 = no output. 1 = only mesh refinement. 2 = NLP solver iteration output and mesh refinement
 
@@ -144,7 +160,7 @@ output = gpops2(setup);
 end
 
 function output = Cost(input)
-P0 = 1;
+global P0;
 stm = input.phase(1).finalstate(2);
 Pf = stm*P0*stm;
 output.objective = Pf;
@@ -153,6 +169,7 @@ output.objective = Pf;
 end
 
 function output = Dynamics(input)
+global P0 CLOSED_LOOP
 
 t = input.phase(1).time;
 s = input.phase(1).state;
@@ -162,19 +179,56 @@ control1 = input.phase(1).control;
 x     = s(:,1);
 stm   = s(:,2);
 
-% Trigonometric function in system dynamics 
+% Trigonometric function in system dynamics
 % dx = sin(x) + (1+0*x.^2).*control1;
 % dstm = (cos(x) + 0*2*x.*control1).*stm;
 
-dx = -x.^3 + control1;
-dstm = -3*x.^2.*stm;
+dx = -dabs(x).*x + control1;
+A = -2*dabs(x);
+B = 1;
+dstm = A.*stm;
 
 % Output
 % output(1).integrand = aT;
+
+if CLOSED_LOOP
+    
+    Qf = 1;
+    Q = 0;
+    R = 0.1;
+    K = t*0;
+    S = Qf;
+    dt = diff(t);
+%     S = Qf./stm.^2;
+%     K = -B*S/R;
+    for i = length(t)-1:-1:1
+%         Ad = 1 + dt(i)*A(i);
+%         Bd = B*dt(i);
+%         K(i) = -Bd*S*Ad/(R + Bd*S*Bd);
+%         S = Ad^2*S + (Ad*S*Bd)*K(i) + Q;
+                if isnan(A(i)) || isnan(t(i))
+                    K(i) = nan;
+                else
+                    K(i) = -lqr(A(i), B, Qf*t(i)/t(end), R);
+                end
+    end
+    K(end) = K(end-1);
+%         K = -1;
+    dstm = (A+B*K).*stm;
+    P = stm.^2 * P0;
+    std = sqrt(P);
+    delta = 3*std;
+    u1 = control1 + K.*delta;
+    u2 = control1 + -K.*delta;
+    output(1).path = [u1, u2];
+    output(1).gains = K;
+    output(1).std = delta;
+end
+
 output(1).dynamics = [dx dstm];
-% output(1).path = (sum(control1(:,2:4).^2,2)); %Norm of the control directions
 
 end
 
-
-
+function y= dabs(x)
+    y = sqrt(x.^2 + 1e-9);
+end
