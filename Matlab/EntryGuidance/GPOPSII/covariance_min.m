89c0e906907a964162bb83e71766d26aa766f7c8
cdnoyes <aero.noyes@gmail.com>
2018-12-02 18:02:36 -0800

    Improvements to cov min

--- Matlab/EntryGuidance/GPOPSII/covariance_min.m
+++ Matlab/EntryGuidance/GPOPSII/covariance_min.m
@@ -8,8 +8,8 @@ end
 clc;
 
 global P0 CLOSED_LOOP
-P0 = 0.0069; % 0.0069= +/-0.25 3-sigma
-CLOSED_LOOP = true;
+P0 = 0.0069*3; % 0.0069= +/-0.25 3-sigma
+CLOSED_LOOP = false;
 
 output = SolveOCP();
 
@@ -31,11 +31,13 @@ H = sol.costate(:,1).*dx + sol.costate(:,2).*dstm;
 figure(1)
 hold all
 plot(sol.time,sol.state(:,1))
-plot(sol.time,sol.state(:,1)+3*std,'k--')
-plot(sol.time,sol.state(:,1)-3*std,'k--')
-
-legend('x', '\pm 3\sigma')
-title('States')
+plot(sol.time,sol.state(:,1)+3*std,'k')
+plot(sol.time, 1.8*(sol.time+1e-10)./(sol.time+1e-10), 'r--', 'linewidth', 3)
+plot(sol.time,sol.state(:,1)-3*std,'k')
+xlabel('Time')
+ylabel('State')
+legend('x', 'x\pm 3\sigma','constraint')
+% title('States')
 set(gcf,'name','Position vs Time','numbertitle','off')
 set(gcf,'WindowStyle','docked')
 
@@ -80,6 +82,8 @@ if CLOSED_LOOP
 end
 output = output.result.solution;
 
+% save("closed_loop_constrained.mat", "sol")
+
 end
 
 function output = SolveOCP()
@@ -92,8 +96,8 @@ tmax = 1;
 x0 = 1;
 stm0 = 1;
 
-xmin = -10;
-xmax = 10;
+xmin = -1.8;
+xmax = 1.8;
 
 xf = 0;
 umax =  3;
@@ -117,18 +121,24 @@ bounds.phase(iphase).finalstate.upper          = [xf 1000];
 
 bounds.phase(iphase).control.lower = [-umax];
 bounds.phase(iphase).control.upper = [umax];
+
 if CLOSED_LOOP
-    bounds.phase(iphase).path.lower = [-umax, -umax];
-    bounds.phase(iphase).path.upper = [umax, umax];
+    bounds.phase(iphase).path.lower = [-1.8, -1.8, -umax, -umax];
+    bounds.phase(iphase).path.upper = [1.8, 1.8, umax, umax];
 end
 % bounds.phase(iphase).integral.lower = 0;
 % bounds.phase(iphase).integral.upper = tmax*amax^2;
 
 %% Guess
+if 0
 guess.phase(iphase).time           = [0; tmax]; % 0.5*(tmin+tmax)
 guess.phase(iphase).state     = [x0, 1; xf, 1e-3];
 guess.phase(iphase).control   = [3; -3];
 % guess.phase(iphase).integral = [ 0.5*(amin+amax)*15];
+else
+   load open_loop_constrained.mat 
+   guess.phase = sol;
+end
 
 %% NLP Parameters and Optimization Call
 setup.name = 'VarMin';
@@ -150,7 +160,7 @@ setup.derivatives.derivativelevel = 'first'; % 'first' or 'second'
 setup.derivatives.dependencies = 'sparseNaN'; % 'full', 'sparse' or 'sparseNaN'
 setup.scales.method = 'automatic-guessUpdate'; % 'none' or 'automatic-bounds' or 'automatic-guess' or 'automatic-guessUpdate' or 'automatic-hybrid' or 'automatic-hybridUpdate' or 'defined'
 setup.mesh.method = 'hp-PattersonRao'; % 'hp-PattersonRao' or 'hp-DarbyRao' or 'hp-LiuRao'
-setup.mesh.tolerance = 1e-5; % Default 1e-3
+setup.mesh.tolerance = 1e-8; % Default 1e-3
 setup.mesh.maxiterations = 30; % Default 10
 setup.method = 'RPM-Differentiation'; % 'RPM-Differentiation' or 'RPM-Integration'
 setup.displaylevel = 1; % 0 = no output. 1 = only mesh refinement. 2 = NLP solver iteration output and mesh refinement
@@ -182,45 +192,45 @@ stm   = s(:,2);
 % Trigonometric function in system dynamics
 % dx = sin(x) + (1+0*x.^2).*control1;
 % dstm = (cos(x) + 0*2*x.*control1).*stm;
-
-dx = -dabs(x).*x + control1;
-A = -2*dabs(x);
+alpha = 0.35;
+dx = -alpha*dabs(x).*x + control1;
+A = -2*alpha*dabs(x);
 B = 1;
 dstm = A.*stm;
 
-% Output
-% output(1).integrand = aT;
 
 if CLOSED_LOOP
     
-    Qf = 1;
+    Qf = 10;
     Q = 0;
     R = 0.1;
     K = t*0;
     S = Qf;
     dt = diff(t);
-%     S = Qf./stm.^2;
-%     K = -B*S/R;
-    for i = length(t)-1:-1:1
-%         Ad = 1 + dt(i)*A(i);
-%         Bd = B*dt(i);
-%         K(i) = -Bd*S*Ad/(R + Bd*S*Bd);
-%         S = Ad^2*S + (Ad*S*Bd)*K(i) + Q;
-                if isnan(A(i)) || isnan(t(i))
-                    K(i) = nan;
-                else
-                    K(i) = -lqr(A(i), B, Qf*t(i)/t(end), R);
-                end
-    end
-    K(end) = K(end-1);
-%         K = -1;
+%     for i = length(t)-1:-1:1
+%         if false % Finite horizon, discrete implementation 
+%             Ad = 1 + dt(i)*A(i);
+%             Bd = B*dt(i);
+%             K(i) = -Bd*S*Ad/(R + Bd*S*Bd);
+%             S = Ad^2*S + (Ad*S*Bd)*K(i) + Q;
+%         else
+%             if isnan(A(i)) || isnan(t(i))
+%                 K(i) = nan;
+%             else
+%                 K(i) = -lqr(A(i), B, Qf, R);
+%             end
+%         end
+%     end
+%     K(end) = K(end-1);
+            K = -1;
+%         K = 0; % This allows for open loop covariance, still respecting state bounds
     dstm = (A+B*K).*stm;
     P = stm.^2 * P0;
     std = sqrt(P);
     delta = 3*std;
     u1 = control1 + K.*delta;
     u2 = control1 + -K.*delta;
-    output(1).path = [u1, u2];
+    output(1).path = [x+delta, x-delta, u1, u2];
     output(1).gains = K;
     output(1).std = delta;
 end
@@ -230,5 +240,5 @@ output(1).dynamics = [dx dstm];
 end
 
 function y= dabs(x)
-    y = sqrt(x.^2 + 1e-9);
+y = sqrt(x.^2 + 1e-9);
 end
